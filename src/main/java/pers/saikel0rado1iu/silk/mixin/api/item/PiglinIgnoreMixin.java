a5b46aca450725223f991bc2947ff4b12a542305
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-09-26 18:16:37 +0800

    fix bug

--- src/main/java/pers/saikel0rado1iu/silk/mixin/api/item/PiglinIgnoreMixin.java
+++ src/main/java/pers/saikel0rado1iu/silk/mixin/api/item/PiglinIgnoreMixin.java
@@ -35,18 +35,15 @@ import static net.minecraft.entity.player.PlayerInventory.MAIN_SIZE;
 abstract class PiglinIgnoreMixin {
 	@Inject(method = "wearsGoldArmor", at = @At("RETURN"), cancellable = true)
 	private static void hasPiglinIgnoreItem(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
-		boolean anySlot = false;
+		boolean notInArmorSlot = false;
 		for (EquipmentSlot slot : EquipmentSlot.values()) {
 			if (!(entity.getEquippedStack(slot).getItem() instanceof PiglinIgnore item)) continue;
-			if ((anySlot = item.getEffectiveEquipmentSlot().isEmpty()) || item.getEffectiveEquipmentSlot()
+			if ((notInArmorSlot = item.getEffectiveEquipmentSlot().isEmpty()) || item.getEffectiveEquipmentSlot()
 					.get().stream().allMatch(equipmentSlot -> equipmentSlot != slot)) continue;
 			cir.setReturnValue(true);
 			return;
 		}
-		if (anySlot) {
-			cir.setReturnValue(true);
-			return;
-		}
+		if (notInArmorSlot) return;
 		if (!(entity instanceof PlayerEntity player)) return;
 		for (int count = 0; count < MAIN_SIZE; count++) {
 			ItemStack stack = player.getInventory().getStack(count);
