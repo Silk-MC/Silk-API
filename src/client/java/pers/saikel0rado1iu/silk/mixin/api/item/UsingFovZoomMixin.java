89f7a920239573e25aa9bc4c5bc0f39df4e70bf9
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-10-15 17:20:56 +0800

    fix issue with mouse movement speed when changing fov

--- src/client/java/pers/saikel0rado1iu/silk/mixin/api/item/UsingFovZoomMixin.java
+++ src/client/java/pers/saikel0rado1iu/silk/mixin/api/item/UsingFovZoomMixin.java
@@ -92,13 +92,13 @@ interface UsingFovZoomMixin {
 			if (fovZoom.onlyFirstPerson()) {
 				if (client.options.getPerspective().isFirstPerson()) {
 					float pullProgress = fovZoom.getUsingProgress(activeStack.getMaxUseTime() - player.getItemUseTimeLeft(), activeStack);
-					float moveMultiple = (float) (Math.pow(fovZoom.getUsingFovMultiple() * pullProgress, 3));
+					float moveMultiple = (float) (Math.pow(1 - (1 - fovZoom.getUsingFovMultiple()) * pullProgress, 3));
 					args.set(0, (double) args.get(0) * moveMultiple);
 					args.set(1, (double) args.get(1) * moveMultiple);
 				}
 			} else {
 				float pullProgress = fovZoom.getUsingProgress(activeStack.getMaxUseTime() - player.getItemUseTimeLeft(), activeStack);
-				float moveMultiple = (float) (Math.pow(fovZoom.getUsingFovMultiple() * pullProgress, 3));
+				float moveMultiple = (float) (Math.pow(1 - (1 - fovZoom.getUsingFovMultiple()) * pullProgress, 3));
 				args.set(0, (double) args.get(0) * moveMultiple);
 				args.set(1, (double) args.get(1) * moveMultiple);
 			}
@@ -114,13 +114,13 @@ interface UsingFovZoomMixin {
 			if (fovZoom.onlyFirstPerson()) {
 				if (client.options.getPerspective().isFirstPerson()) {
 					float pullProgress = fovZoom.getUsingProgress(activeStack.getMaxUseTime() - player.getItemUseTimeLeft(), activeStack);
-					float moveMultiple = (float) (Math.pow(fovZoom.getUsingFovMultiple() * pullProgress, 3));
+					float moveMultiple = (float) (Math.pow(1 - (1 - fovZoom.getUsingFovMultiple()) * pullProgress, 3));
 					args.set(0, (double) args.get(0) * moveMultiple);
 					args.set(1, (double) args.get(1) * moveMultiple);
 				}
 			} else {
 				float pullProgress = fovZoom.getUsingProgress(activeStack.getMaxUseTime() - player.getItemUseTimeLeft(), activeStack);
-				float moveMultiple = (float) (Math.pow(fovZoom.getUsingFovMultiple() * pullProgress, 3));
+				float moveMultiple = (float) (Math.pow(1 - (1 - fovZoom.getUsingFovMultiple()) * pullProgress, 3));
 				args.set(0, (double) args.get(0) * moveMultiple);
 				args.set(1, (double) args.get(1) * moveMultiple);
 			}
