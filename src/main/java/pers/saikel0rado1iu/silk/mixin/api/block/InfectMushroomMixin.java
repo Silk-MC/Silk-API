593a004a5a02e050afadb97618f957b88895615a
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-10-28 22:28:59 +0800

    change some callback

--- src/main/java/pers/saikel0rado1iu/silk/mixin/api/block/InfectMushroomMixin.java
+++ src/main/java/pers/saikel0rado1iu/silk/mixin/api/block/InfectMushroomMixin.java
@@ -9,34 +9,33 @@
  * You should have received a copy of the GNU General Public License along with Silk API. If not, see <https://www.gnu.org/licenses/>.
  */
 
-package pers.saikel0rado1iu.silk.mixin.api.callback;
+package pers.saikel0rado1iu.silk.mixin.api.block;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.SaplingBlock;
+import net.minecraft.block.MushroomPlantBlock;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import pers.saikel0rado1iu.silk.api.callback.InfectSaplingCallback;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import pers.saikel0rado1iu.silk.api.block.InfectMushroom;
 
 /**
- * <p><b style="color:FFC800"><font size="+1">设置树苗成长感染回调</font></b></p>
+ * <p><b style="color:FFC800"><font size="+1">设置菌类成长感染回调</font></b></p>
  * <style="color:FFC800">
  *
  * @author <a href="https://github.com/Saikel-Orado-Liu"><img src="https://avatars.githubusercontent.com/u/88531138?s=64&v=4"><p>
  * @since 0.1.0
  */
-@Mixin(SaplingBlock.class)
-abstract class InfectSaplingMixin {
-	@Inject(method = "generate", at = @At(value = "INVOKE",
-			target = "L net/minecraft/block/sapling/SaplingGenerator;generate(L net/minecraft/server/world/ServerWorld;L net/minecraft/world/gen/chunk/ChunkGenerator;L net/minecraft/util/math/BlockPos;L net/minecraft/block/BlockState;L net/minecraft/util/math/random/Random;)Z"),
+@Mixin(MushroomPlantBlock.class)
+abstract class InfectMushroomMixin {
+	@Inject(method = "trySpawningBigMushroom", at = @At(value = "INVOKE",
+			target = "L net/minecraft/world/gen/feature/ConfiguredFeature;generate(L net/minecraft/world/StructureWorldAccess;L net/minecraft/world/gen/chunk/ChunkGenerator;L net/minecraft/util/math/random/Random;L net/minecraft/util/math/BlockPos;)Z"),
 			cancellable = true)
-	private void generate(ServerWorld world, BlockPos pos, BlockState state, Random random, CallbackInfo ci) {
-		ActionResult result = InfectSaplingCallback.EVENT.invoker().interact(world, pos, state, random, (SaplingBlock) (Object) this);
-		if (result == ActionResult.SUCCESS) ci.cancel();
+	private void trySpawningBigMushroom(ServerWorld world, BlockPos pos, BlockState state, Random random, CallbackInfoReturnable<Boolean> cir) {
+		if (world.getBlockState(pos.down()).getBlock() instanceof InfectMushroom infectMushroom)
+			cir.setReturnValue(infectMushroom.generate(world, pos, state, random, (MushroomPlantBlock) (Object) this));
 	}
 }
--- src/main/java/pers/saikel0rado1iu/silk/mixin/api/block/InfectSaplingMixin.java
+++ src/main/java/pers/saikel0rado1iu/silk/mixin/api/block/InfectSaplingMixin.java
@@ -9,34 +9,35 @@
  * You should have received a copy of the GNU General Public License along with Silk API. If not, see <https://www.gnu.org/licenses/>.
  */
 
-package pers.saikel0rado1iu.silk.mixin.api.callback;
+package pers.saikel0rado1iu.silk.mixin.api.block;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.MushroomPlantBlock;
+import net.minecraft.block.SaplingBlock;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import pers.saikel0rado1iu.silk.api.callback.InfectMushroomCallback;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pers.saikel0rado1iu.silk.api.block.InfectSapling;
 
 /**
- * <p><b style="color:FFC800"><font size="+1">设置菌类成长感染回调</font></b></p>
+ * <p><b style="color:FFC800"><font size="+1">设置树苗成长感染回调</font></b></p>
  * <style="color:FFC800">
  *
  * @author <a href="https://github.com/Saikel-Orado-Liu"><img src="https://avatars.githubusercontent.com/u/88531138?s=64&v=4"><p>
  * @since 0.1.0
  */
-@Mixin(MushroomPlantBlock.class)
-abstract class InfectMushroomMixin {
-	@Inject(method = "trySpawningBigMushroom", at = @At(value = "INVOKE",
-			target = "L net/minecraft/world/gen/feature/ConfiguredFeature;generate(L net/minecraft/world/StructureWorldAccess;L net/minecraft/world/gen/chunk/ChunkGenerator;L net/minecraft/util/math/random/Random;L net/minecraft/util/math/BlockPos;)Z"),
+@Mixin(SaplingBlock.class)
+abstract class InfectSaplingMixin {
+	@Inject(method = "generate", at = @At(value = "INVOKE",
+			target = "L net/minecraft/block/sapling/SaplingGenerator;generate(L net/minecraft/server/world/ServerWorld;L net/minecraft/world/gen/chunk/ChunkGenerator;L net/minecraft/util/math/BlockPos;L net/minecraft/block/BlockState;L net/minecraft/util/math/random/Random;)Z"),
 			cancellable = true)
-	private void trySpawningBigMushroom(ServerWorld world, BlockPos pos, BlockState state, Random random, CallbackInfoReturnable<Boolean> cir) {
-		ActionResult result = InfectMushroomCallback.EVENT.invoker().interact(world, pos, state, random, (MushroomPlantBlock) (Object) this);
-		if (result == ActionResult.SUCCESS) cir.setReturnValue(true);
+	private void generate(ServerWorld world, BlockPos pos, BlockState state, Random random, CallbackInfo ci) {
+		if (world.getBlockState(pos.down()).getBlock() instanceof InfectSapling infectSapling) {
+			infectSapling.generate(world, pos, state, random, (SaplingBlock) (Object) this);
+			ci.cancel();
+		}
 	}
 }
