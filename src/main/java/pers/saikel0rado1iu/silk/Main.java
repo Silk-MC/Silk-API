fab87198e6ab946099e10d09713b4af84563e52b
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-11-03 01:02:12 +0800

    update Main.java

---
+++ src/main/java/pers/saikel0rado1iu/silk/Main.java
@@ -0,0 +1,45 @@
+/*
+ * This file is part of Silk API.
+ * Copyright (C) 2023 Saikel Orado Liu
+ *
+ * Silk API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Silk API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Silk API. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package pers.saikel0rado1iu.silk;
+
+import pers.saikel0rado1iu.silk.api.ModBasicData;
+import pers.saikel0rado1iu.silk.api.ModMain;
+import pers.saikel0rado1iu.silk.api.criterion.SilkCriteria;
+import pers.saikel0rado1iu.silk.api.registry.SilkCriterion;
+
+/**
+ * <p><b style="color:FFC800"><font size="+1">注册 Silk 的必要数据</font></b></p>
+ * <style="color:FFC800">
+ *
+ * @author <a href="https://github.com/Saikel-Orado-Liu"><img src="https://avatars.githubusercontent.com/u/88531138?s=64&v=4"><p>
+ * @since 0.1.0
+ */
+public final class Main extends ModMain {
+	/**
+	 * 此构造方法仅用于父级构造（super() 构造方法），不可在子类实现
+	 */
+	public Main() {
+		super(Silk.DATA);
+	}
+	
+	/**
+	 * 最先运行的模组初始化函数
+	 */
+	@Override
+	public void main(ModBasicData mod) {
+	}
+	
+	@Override
+	public SilkCriterion criteria() {
+		return new SilkCriteria();
+	}
+}
