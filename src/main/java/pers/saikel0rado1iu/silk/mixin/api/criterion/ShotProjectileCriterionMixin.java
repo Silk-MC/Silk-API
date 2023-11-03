1925f5a12b48a808f3a43863981d9cb26246340f
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-11-03 22:42:42 +0800

    update CriterionMixin

--- src/main/java/pers/saikel0rado1iu/silk/mixin/api/criterion/ShotProjectileCriterionMixin.java
+++ src/main/java/pers/saikel0rado1iu/silk/mixin/api/criterion/ShotProjectileCriterionMixin.java
@@ -11,17 +11,13 @@
 
 package pers.saikel0rado1iu.silk.mixin.api.criterion;
 
-import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -29,60 +25,46 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import pers.saikel0rado1iu.silk.api.criterion.RangedKilledEntityCriterion;
 import pers.saikel0rado1iu.silk.api.criterion.SilkCriteria;
 
 /**
- * <p><b style="color:FFC800"><font size="+1">设置远程武器击杀实体标准触发</font></b></p>
+ * <p><b style="color:FFC800"><font size="+1">设置射击弹药标准在原版中弓弩可触发</font></b></p>
  * <style="color:FFC800">
  *
  * @author <a href="https://github.com/Saikel-Orado-Liu"><img src="https://avatars.githubusercontent.com/u/88531138?s=64&v=4"><p>
  * @since 0.1.0
  */
-interface RangedKilledEntityCriterionMixin {
-	@Mixin(ServerPlayerEntity.class)
-	abstract class Trigger extends PlayerEntity {
-		private Trigger(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
-			super(world, pos, yaw, gameProfile);
-		}
-		
-		@Inject(method = "updateKilledAdvancementCriterion", at = @At("TAIL"))
-		private void updateKilledAdvancementCriterion(Entity entityKilled, int score, DamageSource damageSource, CallbackInfo ci) {
-			SilkCriteria.RANGED_KILLED_ENTITY_CRITERION.trigger((ServerPlayerEntity) (Object) this, entityKilled, damageSource);
-		}
-	}
-	
+interface ShotProjectileCriterionMixin {
 	@Mixin(BowItem.class)
 	abstract class Bow {
 		@Unique
-		private ItemStack stack;
-		
-		@Inject(method = "onStoppedUsing", at = @At("HEAD"))
-		private void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
-			this.stack = stack;
-		}
+		private Entity persistentProjectileEntity;
 		
 		@ModifyArg(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "L net/minecraft/world/World;spawnEntity(L net/minecraft/entity/Entity;)Z"))
 		private Entity getEntity(Entity entity) {
-			RangedKilledEntityCriterion.putRangedNbt(entity, stack);
-			return entity;
+			return persistentProjectileEntity = entity;
+		}
+		
+		@Inject(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "L net/minecraft/world/World;spawnEntity(L net/minecraft/entity/Entity;)Z", shift = At.Shift.AFTER))
+		private void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
+			if (user instanceof ServerPlayerEntity serverPlayer)
+				SilkCriteria.SHOT_PROJECTILE_CRITERION.trigger(serverPlayer, stack, persistentProjectileEntity, 1);
 		}
 	}
 	
 	@Mixin(CrossbowItem.class)
 	abstract class Crossbow {
 		@Unique
-		private static ItemStack stack;
-		
-		@Inject(method = "shoot", at = @At("HEAD"))
-		private static void shoot(World world, LivingEntity shooter, Hand hand, ItemStack crossbow, ItemStack projectile, float soundPitch, boolean creative, float speed, float divergence, float simulated, CallbackInfo ci) {
-			stack = crossbow;
-		}
+		private static Entity projectileEntity;
 		
 		@ModifyArg(method = "shoot", at = @At(value = "INVOKE", target = "L net/minecraft/world/World;spawnEntity(L net/minecraft/entity/Entity;)Z"))
 		private static Entity getEntity(Entity entity) {
-			RangedKilledEntityCriterion.putRangedNbt(entity, stack);
-			return entity;
+			return projectileEntity = entity;
+		}
+		
+		@Inject(method = "shoot", at = @At("TAIL"))
+		private static void shoot(World world, LivingEntity shooter, Hand hand, ItemStack crossbow, ItemStack projectile, float soundPitch, boolean creative, float speed, float divergence, float simulated, CallbackInfo ci) {
+			if (shooter instanceof ServerPlayerEntity serverPlayer) SilkCriteria.SHOT_PROJECTILE_CRITERION.trigger(serverPlayer, crossbow, projectileEntity, 1);
 		}
 	}
 }
