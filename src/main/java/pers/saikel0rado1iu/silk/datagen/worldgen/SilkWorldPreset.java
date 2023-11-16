7b440696044ec7dbec213fa04bc1783c7d63596e
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-11-16 19:47:01 +0800

    add class Registrar

--- src/main/java/pers/saikel0rado1iu/silk/datagen/worldgen/SilkWorldPreset.java
+++ src/main/java/pers/saikel0rado1iu/silk/datagen/worldgen/SilkWorldPreset.java
@@ -12,14 +12,31 @@
 package pers.saikel0rado1iu.silk.datagen.worldgen;
 
 import net.minecraft.registry.Registerable;
+import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.structure.StructureSet;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
+import net.minecraft.world.biome.source.MultiNoiseBiomeSourceParameterList;
+import net.minecraft.world.biome.source.MultiNoiseBiomeSourceParameterLists;
+import net.minecraft.world.biome.source.TheEndBiomeSource;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.dimension.DimensionTypes;
 import net.minecraft.world.gen.WorldPreset;
 import net.minecraft.world.gen.WorldPresets;
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
+import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.jetbrains.annotations.ApiStatus;
+import pers.saikel0rado1iu.silk.annotation.SilkApi;
 import pers.saikel0rado1iu.silk.api.ModBasicData;
 import pers.saikel0rado1iu.silk.datagen.SilkWorldGenerator;
+
+import java.util.Map;
 
 /**
  * <p><b style="color:FFC800"><font size="+1">用于创建、注册、生成世界预设</font></b></p>
@@ -46,4 +63,43 @@ public abstract class SilkWorldPreset {
 	 */
 	@ApiStatus.OverrideOnly
 	public abstract void bootstrap(Registerable<WorldPreset> presetRegisterable);
+	
+	@SilkApi
+	public static class Registrar {
+		public final Registerable<WorldPreset> presetRegisterable;
+		public final RegistryEntryLookup<ChunkGeneratorSettings> chunkGeneratorSettingsLookup;
+		public final RegistryEntryLookup<Biome> biomeLookup;
+		public final RegistryEntryLookup<PlacedFeature> featureLookup;
+		public final RegistryEntryLookup<StructureSet> structureSetLookup;
+		public final RegistryEntryLookup<MultiNoiseBiomeSourceParameterList> multiNoisePresetLookup;
+		public final RegistryEntry<DimensionType> overworldDimensionType;
+		public final DimensionOptions netherDimensionOptions;
+		public final DimensionOptions endDimensionOptions;
+		
+		Registrar(Registerable<WorldPreset> presetRegisterable) {
+			this.presetRegisterable = presetRegisterable;
+			RegistryEntryLookup<DimensionType> registryEntryLookup = presetRegisterable.getRegistryLookup(RegistryKeys.DIMENSION_TYPE);
+			chunkGeneratorSettingsLookup = presetRegisterable.getRegistryLookup(RegistryKeys.CHUNK_GENERATOR_SETTINGS);
+			biomeLookup = presetRegisterable.getRegistryLookup(RegistryKeys.BIOME);
+			featureLookup = presetRegisterable.getRegistryLookup(RegistryKeys.PLACED_FEATURE);
+			structureSetLookup = presetRegisterable.getRegistryLookup(RegistryKeys.STRUCTURE_SET);
+			multiNoisePresetLookup = presetRegisterable.getRegistryLookup(RegistryKeys.MULTI_NOISE_BIOME_SOURCE_PARAMETER_LIST);
+			overworldDimensionType = registryEntryLookup.getOrThrow(DimensionTypes.OVERWORLD);
+			RegistryEntry.Reference<DimensionType> registryEntry = registryEntryLookup.getOrThrow(DimensionTypes.THE_NETHER);
+			RegistryEntry.Reference<ChunkGeneratorSettings> registryEntry2 = chunkGeneratorSettingsLookup.getOrThrow(ChunkGeneratorSettings.NETHER);
+			RegistryEntry.Reference<MultiNoiseBiomeSourceParameterList> reference = multiNoisePresetLookup.getOrThrow(MultiNoiseBiomeSourceParameterLists.NETHER);
+			netherDimensionOptions = new DimensionOptions(registryEntry, new NoiseChunkGenerator(MultiNoiseBiomeSource.create(reference), registryEntry2));
+			RegistryEntry.Reference<DimensionType> registryEntry3 = registryEntryLookup.getOrThrow(DimensionTypes.THE_END);
+			RegistryEntry.Reference<ChunkGeneratorSettings> registryEntry4 = chunkGeneratorSettingsLookup.getOrThrow(ChunkGeneratorSettings.END);
+			endDimensionOptions = new DimensionOptions(registryEntry3, new NoiseChunkGenerator(TheEndBiomeSource.createVanilla(biomeLookup), registryEntry4));
+		}
+		
+		public WorldPreset createPreset(DimensionOptions dimensionOptions) {
+			return new WorldPreset(Map.of(DimensionOptions.OVERWORLD, dimensionOptions, DimensionOptions.NETHER, netherDimensionOptions, DimensionOptions.END, endDimensionOptions));
+		}
+		
+		public void register(RegistryKey<WorldPreset> key, DimensionOptions dimensionOptions) {
+			presetRegisterable.register(key, createPreset(dimensionOptions));
+		}
+	}
 }
