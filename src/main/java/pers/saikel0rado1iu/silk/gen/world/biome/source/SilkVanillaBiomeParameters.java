50651d9b45e66038334e36fbea465e1086bec9b8
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-11-20 19:19:38 +0800

    update SilkVanillaBiomeParameters.java

---
+++ src/main/java/pers/saikel0rado1iu/silk/gen/world/biome/source/SilkVanillaBiomeParameters.java
@@ -0,0 +1,26 @@
+/*
+ * This file is part of Silk API.
+ * Copyright (C) 2023 Saikel Orado Liu
+ *
+ * Silk API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Silk API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Silk API. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package pers.saikel0rado1iu.silk.gen.world.biome.source;
+
+import net.minecraft.world.biome.source.util.VanillaBiomeParameters;
+import pers.saikel0rado1iu.silk.annotation.SilkApi;
+
+/**
+ * <p><b style="color:FFC800"><font size="+1">用于继承原版的生物群系参数以创造在拓展原版主维度的模组维度</font></b></p>
+ * <style="color:FFC800">
+ *
+ * @author <a href="https://github.com/Saikel-Orado-Liu"><img src="https://avatars.githubusercontent.com/u/88531138?s=64&v=4"><p>
+ * @since 0.1.0
+ */
+@SilkApi
+public abstract class SilkVanillaBiomeParameters extends VanillaBiomeParameters {
+}
