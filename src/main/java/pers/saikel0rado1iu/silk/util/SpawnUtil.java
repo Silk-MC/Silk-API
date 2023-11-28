1cb2b5a3a68ef7943fd143bf3ddf3f6b90d1cd10
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-11-28 20:55:17 +0800

    update SpawnUtil.java

--- src/main/java/pers/saikel0rado1iu/silk/util/SpawnUtil.java
+++ src/main/java/pers/saikel0rado1iu/silk/util/SpawnUtil.java
@@ -11,11 +11,14 @@
 
 package pers.saikel0rado1iu.silk.util;
 
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.LightType;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 import net.minecraft.world.biome.Biome;
 import pers.saikel0rado1iu.silk.annotation.SilkApi;
 
@@ -25,6 +28,8 @@ import java.util.function.Function;
  * <p><b style="color:FFC800"><font size="+1">有关控制生物的特殊生成的部分数值与实用方法</font></b></p>
  * <p style="color:FFC800">此方法主要用于类似怪物类生物的生成，而不是动物类生物的生成。
  * 如果要实现动物类的生成要在 {@link Builder#otherChecker(SpawnRestriction.SpawnPredicate)} 中手动添加检测器</p>
+ * <p style="color:FF0000">！注意！在特殊情况下，此方法可能看起来失效。此时需要将 {@link MobEntity#canSpawn(WorldView)} 与 {@link MobEntity#canSpawn(WorldAccess, SpawnReason)}
+ * 返回 true 则可以修复此问题。但无法预测此操作可能带来的完整效果，但经过测试，绝大部分情况下并不会破坏生成规则</p>
  * <style="color:FFC800">
  *
  * @author <a href="https://github.com/Saikel-Orado-Liu"><img src="https://avatars.githubusercontent.com/u/88531138?s=64&v=4"><p>
@@ -33,19 +38,19 @@ import java.util.function.Function;
 @SilkApi
 public interface SpawnUtil {
 	@SilkApi
-	static Builder builder() {
-		return new Builder();
+	static <T extends MobEntity> Builder<T> builder(Class<T> tClass) {
+		return new Builder<>();
 	}
 	
 	@SilkApi
-	final class Builder {
+	final class Builder<T extends MobEntity> {
 		private boolean isDay = false;
 		private boolean isNight = false;
 		private boolean isMonster = false;
 		private Function<Long, Boolean> timeChecker = aLong -> true;
 		private Function<Integer, Boolean> lightChecker = aInt -> true;
 		private Function<RegistryEntry<Biome>, Boolean> biomeChecker = biomeRegistryEntry -> true;
-		private SpawnRestriction.SpawnPredicate<MobEntity> otherChecker = (type, world, spawnReason, pos, random) -> true;
+		private SpawnRestriction.SpawnPredicate<T> otherChecker = (type, world, spawnReason, pos, random) -> true;
 		
 		private Builder() {
 		}
@@ -54,7 +59,7 @@ public interface SpawnUtil {
 		 * 如果与 {@link Builder#night()} 一起使用则很可能无法生成生物
 		 */
 		@SilkApi
-		public Builder day() {
+		public Builder<T> day() {
 			isDay = true;
 			return this;
 		}
@@ -63,19 +68,19 @@ public interface SpawnUtil {
 		 * 如果与 {@link Builder#day()} 一起使用则很可能无法生成生物
 		 */
 		@SilkApi
-		public Builder night() {
+		public Builder<T> night() {
 			isNight = true;
 			return this;
 		}
 		
 		@SilkApi
-		public Builder monster() {
+		public Builder<T> monster() {
 			this.isMonster = true;
 			return this;
 		}
 		
 		@SilkApi
-		public Builder time(Function<Long, Boolean> timeChecker) {
+		public Builder<T> time(Function<Long, Boolean> timeChecker) {
 			this.timeChecker = timeChecker;
 			return this;
 		}
@@ -85,25 +90,25 @@ public interface SpawnUtil {
 		 * 如果要进行更精细的控制可以使用 {@link Builder#otherChecker(SpawnRestriction.SpawnPredicate)}
 		 */
 		@SilkApi
-		public Builder light(Function<Integer, Boolean> lightChecker) {
+		public Builder<T> light(Function<Integer, Boolean> lightChecker) {
 			this.lightChecker = lightChecker;
 			return this;
 		}
 		
 		@SilkApi
-		public Builder biome(Function<RegistryEntry<Biome>, Boolean> biomeChecker) {
+		public Builder<T> biome(Function<RegistryEntry<Biome>, Boolean> biomeChecker) {
 			this.biomeChecker = biomeChecker;
 			return this;
 		}
 		
 		@SilkApi
-		public Builder otherChecker(SpawnRestriction.SpawnPredicate<MobEntity> otherChecker) {
+		public Builder<T> otherChecker(SpawnRestriction.SpawnPredicate<T> otherChecker) {
 			this.otherChecker = otherChecker;
 			return this;
 		}
 		
 		@SilkApi
-		public SpawnRestriction.SpawnPredicate<? extends MobEntity> build() {
+		public SpawnRestriction.SpawnPredicate<T> build() {
 			return (type, world, spawnReason, pos, random) -> MobEntity.canMobSpawn(type, world, spawnReason, pos, random)
 					&& biomeChecker.apply(world.getBiome(pos))
 					&& timeChecker.apply(world.getLunarTime())
