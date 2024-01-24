4502dec7173e114fc708b55173d00b2d2f6ef561
GameGeek-Saikel <saikel-orado@foxmail.com>
2024-01-24 20:40:51 +0800

    update new method to use

--- src/main/java/pers/saikel0rado1iu/silk/gen/world/biome/source/SilkVanillaBiomeParameters.java
+++ src/main/java/pers/saikel0rado1iu/silk/gen/world/biome/source/SilkVanillaBiomeParameters.java
@@ -11,8 +11,16 @@
 
 package pers.saikel0rado1iu.silk.gen.world.biome.source;
 
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.util.MultiNoiseUtil;
 import net.minecraft.world.biome.source.util.VanillaBiomeParameters;
+import org.jetbrains.annotations.ApiStatus;
 import pers.saikel0rado1iu.silk.annotation.SilkApi;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * <h2 style="color:FFC800">用于继承原版的生物群系参数以创造在拓展原版主维度的模组维度</h2>
@@ -22,4 +30,20 @@ import pers.saikel0rado1iu.silk.annotation.SilkApi;
  */
 @SilkApi
 public abstract class SilkVanillaBiomeParameters extends VanillaBiomeParameters {
+	@SilkApi
+	public List<RegistryKey<Biome>> getNonVanillaGeneratedBiome() {
+		return List.of();
+	}
+	
+	@Override
+	public void writeOverworldBiomeParameters(Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>> parameters) {
+		super.writeOverworldBiomeParameters(parameters);
+		addNonVanillaGeneratedBiome(parameters);
+	}
+	
+	@ApiStatus.Internal
+	public void addNonVanillaGeneratedBiome(Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>> parameters) {
+		getNonVanillaGeneratedBiome().forEach(biomeRegistryKey -> parameters.accept(Pair.of(
+				MultiNoiseUtil.createNoiseHypercube(9, 9, 9, 9, 9, 9, 9), biomeRegistryKey)));
+	}
 }
