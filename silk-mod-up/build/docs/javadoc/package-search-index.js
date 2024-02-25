71b60a7cc2e4b3a855639628ba330d70f56b5fa8
GameGeek-Saikel <saikel-orado@foxmail.com>
2024-02-25 15:39:38 +0800

    delete build

--- silk-mod-up/build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"pers.saikel0rado1iu.silk.impl"},{"l":"pers.saikel0rado1iu.silk.modup"},{"l":"pers.saikel0rado1iu.silk.util.update"},{"l":"所有程序包","u":"allpackages-index.html"}];updateSearchResults();
